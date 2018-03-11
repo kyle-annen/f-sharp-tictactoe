004eef0307f52239559cb94125ce2a2e6abed767
Kyle Annen <kyleannen@gmail.com>
2018-03-11 17:43:40 -0500

    Add row function

--- src/TicTacToe/Board.fs
+++ src/TicTacToe/Board.fs
@@ -4,18 +4,18 @@ type Space =
     | A 
     | B
     | Empty
-    
+
 type Board = Space list
 
-let initBoard (dimension:int) :Board =
+let initBoard (dimension:int) : Board =
     [ for i in 1..(dimension * dimension) do yield Empty]
 
-let getPreceding (loc:int) (board:Board) :Board =
+let getPreceding (loc:int) (board:Board) : Board =
     board
     |> Seq.take (loc - 1)
     |> Seq.toList
 
-let getFollowing (loc:int) (board:Board) :Board =
+let getFollowing (loc:int) (board:Board) : Board =
     if loc = board.Length then
         []
     else 
@@ -23,17 +23,30 @@ let getFollowing (loc:int) (board:Board) :Board =
         |> Seq.skip (loc)
         |> Seq.toList
 
-let getMove space :Board = [space;]
+let getMove space : Board = [space;]
 
 let placeMove (loc:int) (space:Space) (board:Board) :Board = 
     (getFollowing loc board)
     |> List.append (getMove space)
     |> List.append (getPreceding loc board)
 
+let private openSpace (i, v) = v = Empty
+
+let private indexToLocation (i, v) = i + 1
+
 let getOpenMoves (board:Board) = 
     board
-    |> List.toArray
-    |> Array.indexed
-    |> Array.filter (fun (i, v) -> v = Empty)
-    |> Array.map (fun (i, b) -> i + 1)
-    |> Array.toList
+    |> List.indexed
+    |> List.filter openSpace
+    |> List.map indexToLocation
+
+let checkSeq (board:Board) : bool =  
+    match board.Head with
+    | Empty -> false
+    | _ -> List.forall (fun elem -> elem = board.Head) board
+
+let intSqrt (n:int) : int = 
+    n |> float |> sqrt |> int
+    
+let rows (board:Board) : Board list = 
+    board |> List.chunkBySize (intSqrt board.Length)
