29a233f9d069cdf47a1e8e00686fd0dd89793d54
Kyle Annen <kyleannen@gmail.com>
2018-03-12 11:59:21 -0500

    Add GameState, swapPlayers, AI module

--- src/TicTacToe/AI.fs
+++ src/TicTacToe/AI.fs
@@ -1,10 +1,34 @@
-namespace TicTacToe.AI
+module TicTacToe.AI
 
-type Player =
-  | Human
-  | Computer
+open Board
+
+type PlayerType =
+    | Human
+    | Computer
 
 type Difficulty = 
-  | Easy
-  | Medium
-  | Hard
+    | Easy
+    | Medium
+    | Hard
+
+type Player = {
+    PlayerType: PlayerType;
+    Difficulty: Difficulty;
+    Space: Space;
+}
+
+type GameState = {
+    CurrentPlayer: Player;
+    NextPlayer: Player;
+    Board: Board;
+    Result: Result;
+}
+
+let swapPlayers (state:GameState) =
+    { state with 
+        CurrentPlayer = state.NextPlayer;
+        NextPlayer = state.CurrentPlayer;}
+
+// let negamax (board:Board) : int = 
+//   getOpenMoves board
+//   |> List.head
